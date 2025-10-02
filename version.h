4523a51fb70bfdee16686232c8914d9ddf3216c7
ceinmart <cesar.inacio.martins@gmail.com>
2025-10-02 16:43:30 -0300

    Changelog: - Agora os outputs dos comandos (:cmd status, :cmd help, etc) são enviados diretamente ao cliente TCP conectado, melhorando a experiência do usuário remoto. - Integração aprimorada entre esp32_keyboard.ino e version.h para exibir informações de versão no status.

--- version.h
+++ version.h
@@ -1,8 +1,8 @@
 
 #ifndef VERSION_H
 #define VERSION_H
-#define GIT_COMMIT   "08b0bda"
+#define GIT_COMMIT   "a5ca4b7"
 #define GIT_BRANCH   "master"
-#define GIT_DATE     "Thu Oct 2 14:59:54 2025 -0300"
+#define GIT_DATE     "Thu Oct 2 16:18:57 2025 -0300"
 #define GIT_PATH     ""
 #endif
